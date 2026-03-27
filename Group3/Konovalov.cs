6eee0c1ef41c5c2f5948c522fd1ab0c7432453b0
Konstantin <H1dra5@yandex.ru>
2026-03-27 09:55:02 +0700

    Доделал историю персонажа

--- Group3/Konovalov.cs
+++ Group3/Konovalov.cs
@@ -8,21 +8,26 @@ using System.Threading.Tasks;
 namespace ForkKnights.Group3 {
   public class Konovalov : IKnight {
     string name, power, fromUniverse, species, status;
+    string mage{get; set;}
     int age;
     bool isHeAHero;
 
   public Konovalov() {
+      mage = "Последний оплот";
       name = "Сифил Лифисов";
       power = "Изначально был рождён с очень сильной магией огня, из-за чего сошёл с ума";
       fromUniverse = "Выходец из вселенной, в которой правят драконы и царит средневековье";
-      species = "Человек, рождённый в сливянии дракона и человека";
+      species = "Человек, рождённый в слиянии дракона и человека";
       status = "Является выходцем из богатой семьи";
-      age = 150;
+      age = 15;
       isHeAHero = false;
     }
 
     public string GetJobApplication() {
-      string background = $@"Он был рождён во времена драконов,{name}";
+      string background = $@"Он был рождён во времена драконов,{name}, {fromUniverse}, тут не ценят героев, драконы давно захватили эти земли" +
+                          "и у людей не осталось надежды, из-за нехватки людей драконы стали размножаться с людьми, чтобы использовать их рабочую силу по максимуму" +
+                          $@"и наш {name} {species}, он {power}, но это сыграет роль в будущем, из-за того, его мать страдает от отца Прафизизауруса он ненавидит драконов," +
+                          $@"так как он {status} это позволяет ему изучать магию у самых лучших магов своего континента, являясь {age}-летним";
       return background;
     }
   }
